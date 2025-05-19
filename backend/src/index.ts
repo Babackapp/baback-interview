dae1daa2a6283e73cb3f96cc2ba7196df9d30dc5
ben-ben-ben-ben-ben-ben <benoit@baback.co>
2025-05-19 13:59:02 +0200

    Level2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,35 +4,9 @@ import {type Product, productsStub} from "./products"
 type DiscountGroup = Record<Discount["type"], Discount[]>
 
 const calculateFinalPrice = (basket: Product[], discountCodes: string[] = []): number => {
-    // const discountByCodes = groupDiscountCodes(discountCodes);
-    let total = calcTotal(basket);
-
-    // calcByOneBoughtOneOffer
-    // calcByFixedPrice
-    // calcByPercentage
-
-    return total;
+    return 1;
 }
 
-function calcTotal(products: Product[]): number {
-    return 0;
-}
-
-function groupDiscountCodes(codes: string[]): DiscountGroup {
-    return {} as DiscountGroup
-}
-
-function calcByPercentage(): number {
-    return 0;
-}
-
-function calcByOneBoughtOneOffer(): number {
-    return 0;
-}
-
-function calcByFixedPrice(): number {
-    return 0;
-}
 
 // console.log(calculateFinalPrice(productsStub, ["10_PERCENT_OFF"]))
 
